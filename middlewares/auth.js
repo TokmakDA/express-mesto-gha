207b80de2958b57cf9c11dba65334d810b7ded9e
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-05-21 15:59:48 +0300

    FIX errors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,22 +6,22 @@ module.exports = async (req, res, next) => {
   const err = new UnauthorizedError('Unauthorized Error');
 
   console.log('auth => ');
-  req.user = { _id: '64676a42271b2c7ee35e7c01' }; // для проверка ошибок
+  // req.user = { _id: '64676a42271b2c7ee35e7c01' }; // для проверка ошибок
 
-  // // убеждаемся, что токен присутсвует
-  // if (!token) {
-  //   console.log('auth => нет куки');
-  //   next(err);
-  //   return;
-  // }
+  // убеждаемся, что токен присутсвует
+  if (!token) {
+    console.log('auth => нет куки');
+    next(err);
+    return;
+  }
 
-  // // проверяем
-  // try {
-  //   const payload = await checkToken(token);
-  //   req.user = { _id: payload._id };
-  // } catch {
-  //   console.log('payload => куки не прошел проверку');
-  //   next(err);
-  // }
+  // проверяем
+  try {
+    const payload = await checkToken(token);
+    req.user = { _id: payload._id };
+  } catch {
+    console.log('payload => куки не прошел проверку');
+    next(err);
+  }
   next(); // пропускаем запрос дальше
 };
