7bf4db2354f92a577c71083355ca35dc18e70076
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-05-22 12:18:25 +0300

    regEx

--- utils/schemes.js
+++ utils/schemes.js
@@ -3,14 +3,16 @@ const { celebrate, Joi } = require('celebrate');
 const userSchema = {
   name: Joi.string().min(2).max(30),
   about: Joi.string().min(2).max(30),
-  avatar: Joi.string(),
+  avatar: Joi.string().regex(/(https?:\/\/)\w+?(\S+|W+)?(\w+)?.\w{2,15}\/?/),
   email: Joi.string().email().required(),
   password: Joi.string().min(8).strip().required(),
 };
 
 const cardSchema = {
   name: Joi.string().min(2).max(30).required(),
-  link: Joi.string().required(),
+  link: Joi.string()
+    .required()
+    .regex(/(https?:\/\/)\w+?(\S+|W+)?(\w+)?.\w{2,15}\/?/),
 };
 
 celebrate({
